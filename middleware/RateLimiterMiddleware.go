a30c0215c0e0f4ec4f83c81864b9c8945b9a2a50
何井才 <he2038141467@163.com>
2024-10-16 17:21:34 +0800

    change:修改最新

--- middleware/RateLimiterMiddleware.go
+++ middleware/RateLimiterMiddleware.go
@@ -1,18 +1,24 @@
 package middleware
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/juju/ratelimit"
+	"net/http"
+)
+
 // 注册令牌桶 设置每秒生成令牌数、令牌桶总容量
-//var limiter = ratelimit.NewBucketWithRate(400, 1800)
-//
-//func RateLimiterMiddleware() gin.HandlerFunc {
-//	return func(context *gin.Context) {
-//		if limiter.TakeAvailable(1) < 1 {
-//			context.JSON(http.StatusTooManyRequests, gin.H{
-//				"status":  429,
-//				"message": "压力山大啊,请休息一会再看看吧！",
-//			})
-//			context.Abort()
-//			return
-//		}
-//		context.Next()
-//	}
-//}
+var limiter = ratelimit.NewBucketWithRate(400, 1800)
+
+func RateLimiterMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		if limiter.TakeAvailable(1) < 1 {
+			context.JSON(http.StatusTooManyRequests, gin.H{
+				"status":  429,
+				"message": "压力山大啊,请休息一会再看看吧！",
+			})
+			context.Abort()
+			return
+		}
+		context.Next()
+	}
+}
--- routes/api.go
+++ routes/api.go
@@ -2,16 +2,17 @@ package routes
 
 import (
 	"blogs/application/controller"
+	"blogs/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func WebRoutes(r *gin.Engine) {
-	//r.Use(middleware.RateLimiterMiddleware()) //令牌桶
+	r.Use(middleware.RateLimiterMiddleware()) //令牌桶
 	//r.Use(middleware.LoggingMiddleware())     //用户来源
 	api := r.Group("api")
 	{
 		v1 := api.Group("v1")
-		//v1.Use(middleware.JwtMiddleware())	//jwt鉴权
+		//v1.Use(middleware.JwtMiddleware()) //jwt鉴权
 		v1.POST("/index", controller.Index)
 		v1.POST("/download_user_excel", controller.DownloadUserExcel)
 	}
