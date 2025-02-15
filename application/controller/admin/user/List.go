d94b6a6c1344f0e6d0965079edf97ff322e3fa6b
何井才 <he2038141467@163.com>
2025-02-15 14:45:29 +0800

    add:新增用户excel导出功能

--- application/controller/admin/user/List.go
+++ application/controller/admin/user/List.go
@@ -2,9 +2,11 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/xuri/excelize/v2"
 	Models "golang-blog/application/models"
 	"golang-blog/config"
 	"net/http"
+	"strconv"
 )
 
 func List(ctx *gin.Context) {
@@ -79,8 +81,85 @@ func ListData(ctx *gin.Context) {
 
 // DownLoadExcel 下载用户数据
 func DownLoadExcel(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"code": 0,
-		"msg":  "请求成功！",
-	})
+	//创建一个新excel
+	excel := excelize.NewFile()
+	sheetName := "sheet1" //文件名称
+	defer func() {        //关闭文档
+		if err := excel.Close(); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"status": http.StatusInternalServerError,
+				"msg":    err.Error(),
+			})
+			return
+		}
+	}()
+
+	streamWriter, err := excel.NewStreamWriter(sheetName)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"status1": http.StatusInternalServerError,
+			"msg":     err.Error(),
+		})
+		return
+	}
+	defer func(streamWriter *excelize.StreamWriter) { //处理关闭错误
+		err := streamWriter.Flush()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"status1": http.StatusInternalServerError,
+				"msg":     err.Error(),
+			})
+		}
+	}(streamWriter)
+
+	header := []interface{}{"ID", "用户名", "账号", "性别", "住址", "状态", "创建时间"}
+	err = streamWriter.SetRow("A1", header)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"status2": http.StatusInternalServerError,
+			"msg":     err.Error(),
+		})
+		return
+	}
+
+	// 分批查询数据
+	limit := 200000 // 每次查询 20 万条
+	rowIndex := 2   // Excel 从 2 开始填充数据（1 是表头）
+	var lastID int
+	maxFor := 0
+	for {
+		var users []Models.User
+		result := config.DB.Where("status in ?", []int{1, 2}).
+			Where("id > ?", lastID).
+			Select("id,nickname,user_name,sex,address,status,created_at").
+			Limit(limit).
+			Find(&users)
+
+		if result.Error != nil || len(users) == 0 || maxFor == 5 {
+			break
+		}
+
+		for _, item := range users {
+			row := []interface{}{item.ID, item.Nickname, item.UserName, item.Sex, item.Address, item.Status, item.CreatedAt}
+			cell := "A" + strconv.Itoa(rowIndex)
+			err := streamWriter.SetRow(cell, row)
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"status4": http.StatusInternalServerError, "msg": err.Error()})
+				return
+			}
+			rowIndex++
+		}
+		// 更新 lastID 为当前查询结果中的最大 id
+		lastID = users[len(users)-1].ID
+		maxFor++
+	}
+
+	// 保存 Excel 到内存并返回给客户端
+	ctx.Header("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+	ctx.Header("Content-Disposition", "attachment; filename=users.xlsx")
+	ctx.Header("File-Transfer-Encoding", "binary")
+	if err := excel.Write(ctx.Writer); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+	}
+
 }
--- go.mod
+++ go.mod
@@ -39,7 +39,10 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.4 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -49,14 +52,17 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
+	github.com/xuri/excelize/v2 v2.9.0 // indirect
+	github.com/xuri/nfp v0.0.0-20240318013403-ab9948c2c4a7 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
